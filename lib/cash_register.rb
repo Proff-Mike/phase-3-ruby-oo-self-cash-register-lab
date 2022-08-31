a6f423e893078948fb9d9eaa6bd7755846e2f8d4
Michael Okoe <michael.okoejr@gmail.com>
2022-08-31 16:14:42 +0000

    commit tree f006cac7a047af952039c641b74f66d9f7507540

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,36 @@
 
+
+class CashRegister
+
+  attr_accessor :discount, :total, :item_history, :current_transaction
+
+  def initialize(discount = nil)
+    @discount = discount
+    @total = 0
+    @item_history = []
+  end
+
+  def add_item(item, price, amount=1)
+    self.current_transaction = price * amount
+    self.total += self.current_transaction
+    amount.times {self.item_history << item}
+  end
+
+  def apply_discount
+    if self.discount
+      self.total -= self.total * self.discount / 100
+      "After the discount, the total comes to $#{self.total}."
+    else
+      "There is no discount to apply."
+    end
+  end
+
+  def items
+    self.item_history
+  end 
+
+  def void_last_transaction
+    self.total -= self.current_transaction
+  end 
+
+end 
